3b9718d04f01dc619a5ea8c170669716564dcfd8
Vitaliy Dobriyan <dobriy88@yandex.ru>
2024-08-17 00:15:53 +0300

    nuget info moved to nuspec, cleanup

--- tools/pack.bat
+++ tools/pack.bat
@@ -1,6 +1,7 @@
 @echo off
-set SRC_ROOT=%cd%\..\src
-set PKG=%1
+set PKG=Unicorn.AllureAgent
+set PROJ_PATH=%cd%\..\src\%PKG%\%PKG%
+set VERSION=%1
 set OUT_DIR=%2
 
-dotnet pack %SRC_ROOT%\%PKG%\%PKG%.csproj -o %OUT_DIR% -c Release -p:NuspecFile=%SRC_ROOT%\%PKG%\%PKG%.nuspec
+dotnet pack %PROJ_PATH%.csproj -o %OUT_DIR% -c Release -p:NuspecFile=%PROJ_PATH%.nuspec -p:Version=%VERSION%
